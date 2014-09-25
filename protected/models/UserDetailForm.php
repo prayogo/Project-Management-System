e136cf0f88a77a1b5412dba37c087eb5e8d7c689
prayogo <prayogo@binus.edu>
2014-09-25 18:56:35 +0700

    generate userdatailform model dan generate userdetail form

---
+++ protected/models/UserDetailForm.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * This is the model class for table "msuser".
+ *
+ * The followings are the available columns in table 'msuser':
+ * @property integer $UserId
+ * @property string $Username
+ * @property string $Name
+ * @property string $Email
+ * @property string $Phone
+ * @property string $Password
+ * @property string $Enable
+ * @property string $UserIn
+ * @property string $DateIn
+ * @property string $UserUp
+ * @property string $DateUp
+ */
+class UserDetailForm extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'msuser';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('Username, Name, Email, Phone, Password', 'required'),
+			array('Username, Password, UserIn, UserUp', 'length', 'max'=>50),
+			array('Name', 'length', 'max'=>250),
+			array('Email', 'length', 'max'=>150),
+			array('Phone', 'length', 'max'=>20),
+			array('Enable', 'length', 'max'=>1),
+			array('DateIn, DateUp', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('UserId, Username, Name, Email, Phone, Password, Enable, UserIn, DateIn, UserUp, DateUp', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'UserId' => 'User',
+			'Username' => 'Username',
+			'Name' => 'Name',
+			'Email' => 'Email',
+			'Phone' => 'Phone',
+			'Password' => 'Password',
+			'Enable' => 'Enable',
+			'UserIn' => 'User In',
+			'DateIn' => 'Date In',
+			'UserUp' => 'User Up',
+			'DateUp' => 'Date Up',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('UserId',$this->UserId);
+		$criteria->compare('Username',$this->Username,true);
+		$criteria->compare('Name',$this->Name,true);
+		$criteria->compare('Email',$this->Email,true);
+		$criteria->compare('Phone',$this->Phone,true);
+		$criteria->compare('Password',$this->Password,true);
+		$criteria->compare('Enable',$this->Enable,true);
+		$criteria->compare('UserIn',$this->UserIn,true);
+		$criteria->compare('DateIn',$this->DateIn,true);
+		$criteria->compare('UserUp',$this->UserUp,true);
+		$criteria->compare('DateUp',$this->DateUp,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return UserDetailForm the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
--- protected/views/user/userdetail.php
+++ protected/views/user/userdetail.php
@@ -1,44 +1,89 @@
-<form>  
-    <div class="small-8">
-      <div class="">
-        <div class="small-3 columns">
-          <label for="right-label" class="right inline">User Name</label>
-        </div>
-        <div class="small-9 columns">
-          <input type="text" id="right-label" placeholder="User Name...">
-        </div>
-      </div>
-      <div class="">
-        <div class="small-3 columns">
-          <label for="right-label" class="right inline">Password</label>
-        </div>
-        <div class="small-9 columns">
-          <input type="password" id="right-label" placeholder="Password...">
-        </div>
-      </div>
-      <div class="">
-        <div class="small-3 columns">
-          <label for="right-label" class="right inline">Confirm Password</label>
-        </div>
-        <div class="small-9 columns">
-          <input type="password" id="right-label" placeholder="Confirm Password...">
-        </div>
-      </div>
-      <div class="">
-        <div class="small-3 columns">
-          <label for="right-label" class="right inline">Email</label>
-        </div>
-        <div class="small-9 columns">
-          <input type="email" id="right-label" placeholder="Email...">
-        </div>
-      </div>
-      <div class="">
-        <div class="small-3 columns">
-          <label for="right-label" class="right inline">Phone</label>
-        </div>
-        <div class="small-9 columns">
-          <input type="text" id="right-label" placeholder="Phone...">
-        </div>
-      </div>
-    </div>  
-</form>
+<?php
+/* @var $this UserDetailFormController */
+/* @var $model UserDetailForm */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'user-detail-form-userdetail-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// See class documentation of CActiveForm for details on this,
+	// you need to use the performAjaxValidation()-method described there.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Username'); ?>
+		<?php echo $form->textField($model,'Username'); ?>
+		<?php echo $form->error($model,'Username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Name'); ?>
+		<?php echo $form->textField($model,'Name'); ?>
+		<?php echo $form->error($model,'Name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Email'); ?>
+		<?php echo $form->textField($model,'Email'); ?>
+		<?php echo $form->error($model,'Email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Phone'); ?>
+		<?php echo $form->textField($model,'Phone'); ?>
+		<?php echo $form->error($model,'Phone'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Password'); ?>
+		<?php echo $form->textField($model,'Password'); ?>
+		<?php echo $form->error($model,'Password'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'UserIn'); ?>
+		<?php echo $form->textField($model,'UserIn'); ?>
+		<?php echo $form->error($model,'UserIn'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'UserUp'); ?>
+		<?php echo $form->textField($model,'UserUp'); ?>
+		<?php echo $form->error($model,'UserUp'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'Enable'); ?>
+		<?php echo $form->textField($model,'Enable'); ?>
+		<?php echo $form->error($model,'Enable'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'DateIn'); ?>
+		<?php echo $form->textField($model,'DateIn'); ?>
+		<?php echo $form->error($model,'DateIn'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'DateUp'); ?>
+		<?php echo $form->textField($model,'DateUp'); ?>
+		<?php echo $form->error($model,'DateUp'); ?>
+	</div>
+
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
