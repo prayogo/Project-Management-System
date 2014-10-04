4dd1d0c410878a2b3eb2c5c17d1e5e68d0862e56
sundirosun <sunz_wei@yahoo.com>
2014-10-04 16:57:05 +0700

    user access user group

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -15,85 +15,119 @@ class UserController extends Controller
 
 	public function actionManageuser()
 	{
-		$model=new UserDetailForm;
-		$model->Enable=1;
+		$model_detail=new UserDetailForm;
+		$model_detail->Enable=1;
+
+		$activeTab = "userdetail";
+
+		$model_access=new UserAccessForm;
+		$checkAccess=array();
+		
+		$model_user=new UserGroupForm;
+		$checkGroup=array();
 		
 		if(isset($_POST['UserDetailForm'])){
-			$model->setUsername($_POST['UserDetailForm']['Username']);
+			$model_detail->setUsername($_POST['UserDetailForm']['Username']);
 		}
 
 		if(isset($_POST['ajax']) && $_POST['ajax']==='user-detail-form-userdetail-form')
 		{
-			echo CActiveForm::validate($model);
+			echo CActiveForm::validate($model_detail);
+			echo CActiveForm::validate($model_access);
 			Yii::app()->end();
 		}
 		
 		// user detail form POST
 		if(isset($_POST['UserDetailForm']))
 		{
-			$model->attributes=$_POST['UserDetailForm'];			
-			$model->UserId = $_POST['UserDetailForm']['UserId'];
-			if ($model->UserId != null && $model->UserId != ""){
-				$model->setScenario('update');
+			$model_detail->attributes=$_POST['UserDetailForm'];			
+			$model_detail->UserId = $_POST['UserDetailForm']['UserId'];
+			if ($model_detail->UserId != null && $model_detail->UserId != ""){
+				$model_detail->setScenario('update');
 			}else{
-				$model->setScenario('insert');
+				$model_detail->setScenario('insert');
 			}
 
-			if($model->validate())
+			if($model_detail->validate())
 			{
-				$model->Username = $_POST['UserDetailForm']['Username'];
-				$model->Name = $_POST['UserDetailForm']['Name'];
-				$model->Email = $_POST['UserDetailForm']['Email'];
-				$model->Phone = $_POST['UserDetailForm']['Phone'];
-				$model->Password = $_POST['UserDetailForm']['Password'];				
-				$model->Enable = $_POST['UserDetailForm']['Enable'];				
-				$model->Copy_User = $_POST['UserDetailForm']['Copy_User'];
-				$model->User = $_POST['UserDetailForm']['User'];
+				$model_detail->Username = $_POST['UserDetailForm']['Username'];
+				$model_detail->Name = $_POST['UserDetailForm']['Name'];
+				$model_detail->Email = $_POST['UserDetailForm']['Email'];
+				$model_detail->Phone = $_POST['UserDetailForm']['Phone'];
+				$model_detail->Password = $_POST['UserDetailForm']['Password'];				
+				$model_detail->Enable = $_POST['UserDetailForm']['Enable'];				
+				$model_detail->Copy_User = $_POST['UserDetailForm']['Copy_User'];
+				$model_detail->User = $_POST['UserDetailForm']['User'];
 				
 
 
-				$response = $model->getUserDetail($model->UserId);
+				$response = $model_detail->getUserDetail($model_detail->UserId);
 				
-				if ($model->UserId != null && $model->UserId != ""){					
+				if ($model_detail->UserId != null && $model_detail->UserId != ""){					
 					if (!empty($response) && isset($response[0]['UserId'])){						
-						$response1 = $model->updateUser($model->Username, $model->Name, $model->Email, $model->Phone,$model->Password, $model->Enable, $model->Copy_User, $model->User);
+						$response1 = $model_detail->updateUser($model_detail->Username, $model_detail->Name, $model_detail->Email, $model_detail->Phone, $model_detail->Password, $model_detail->Enable, $model_detail->Copy_User, $model_detail->User);
 						if ($response1['code'] == StandardVariable::CONSTANT_RETURN_SUCCESS){
 							$this->redirect(array('um/user'));
 						}else{
-							$model->addError('request', $response1['exception'][2]);
+							$model_detail->addError('request', $response1['exception'][2]);
 						}
 					}
 					else{
-						$model->addError('request', 'User not exists');
+						$model_detail->addError('request', 'User not exists');
 					}
 				}
 				else{
-					$response1 = $model->insertUser($model->Username, $model->Name, $model->Email, $model->Phone,$model->Password, $model->Enable, $model->Copy_User, $model->User);
+					$response1 = $model_detail->insertUser($model_detail->Username, $model_detail->Name, $model_detail->Email, $model_detail->Phone, $model_detail->Password, $model_detail->Enable, $model_detail->Copy_User, $model_detail->User);
 					if ($response1['code'] == StandardVariable::CONSTANT_RETURN_SUCCESS){						
 						$this->redirect(array('um/user'));
 					}else{
-						$model->addError('request', $response1['exception'][2]);
+						$model_detail->addError('request', $response1['exception'][2]);
 					}
 				}
 			}
 		}else{
 			$paramURL = CHttpRequest::getParam('id');
 			if (isset($paramURL) && $paramURL != null && $paramURL != ""){
-				$response = $model->getUserDetail($paramURL);
+				$response = $model_detail->getUserDetail($paramURL);
 				if (!empty($response) && isset($response[0]['UserId'])){
-					  $model->UserId = $response[0]['UserId'];
-					  $model->Username = $response[0]['Username'];
-					  $model->Name = $response[0]['Name'];
-					  $model->Email = $response[0]['Email'];
-					  $model->Phone = $response[0]['Phone'];					  
-					  $model->Enable = $response[0]['Enable'];
-					  $model->Password = StandardVariable::CONSTANT_PASSWORD;
-					  $model->Confirm_Password = StandardVariable::CONSTANT_PASSWORD;
+					  $model_detail->UserId = $response[0]['UserId'];
+					  $model_detail->Username = $response[0]['Username'];
+					  $model_detail->Name = $response[0]['Name'];
+					  $model_detail->Email = $response[0]['Email'];
+					  $model_detail->Phone = $response[0]['Phone'];					  
+					  $model_detail->Enable = $response[0]['Enable'];
+					  $model_detail->Password = StandardVariable::CONSTANT_PASSWORD;
+					  $model_detail->Confirm_Password = StandardVariable::CONSTANT_PASSWORD;
 				  }
 			}	
 		}
+
+		if (isset($_POST['UserAccessForm']["UserAccess"])){
+			$activeTab = "useraccess";
+			foreach ($_POST['UserAccessForm']["UserAccess"] as $key => $value) {
+				if ($value == "on"){
+					array_push($checkAccess, $key);
+				}
+			}
+		}
 		
-		$this->render('manageuser', array('model'=>$model));
+		if (isset($_POST['UserGroupForm']["UserGroup"])){
+			$activeTab = "usergroup";
+			foreach ($_POST['UserGroupForm']["UserGroup"] as $key => $value) {
+				if ($value == "on"){
+					array_push($checkGroup, $key);
+				}
+			}
+		}
+				
+		$this->render('manageuser', array(
+			'activeTab'=>$activeTab,
+			'model_detail'=>$model_detail, 
+			'model_access'=>$model_access, 
+			'model_user'=>$model_user, 
+			'checkAccess' => $checkAccess,
+			'checkGroup' => $checkGroup
+		));
 	}
 
 	public function actionGetUserList(){
@@ -110,5 +144,33 @@ class UserController extends Controller
 			}
 		}
 		echo json_encode($userList);
+	}
+
+	public function actionUserAccessList(){
+		$model=new UserAccessForm;
+		$UserId = "0";
+		$accessList = array();
+		if (isset($_POST['ajax']) && $_POST['ajax'] != null && $_POST['ajax'] != ""){
+			if (isset($_POST['userid']) && $_POST['userid'] != null && $_POST['userid'] != ""){
+				$UserId = $_POST['UserId'];
+			}
+			
+			$accessList = $model->getUserAccessList($UserId);
+		}
+		echo json_encode($accessList);
+	}
+
+	public function actionUserGroupList(){
+		$model=new UserGroupForm;
+		$UserId = "0";
+		$accessList = array();
+		if (isset($_POST['ajax']) && $_POST['ajax'] != null && $_POST['ajax'] != ""){
+			if (isset($_POST['userid']) && $_POST['userid'] != null && $_POST['userid'] != ""){
+				$UserId = $_POST['UserId'];
+			}
+			
+			$accessList = $model->getUserGroupList($UserId);
+		}
+		echo json_encode($accessList);
 	}
 }
--- protected/views/user/userdetail.php
+++ protected/views/user/userdetail.php
@@ -1,113 +1,96 @@
 <?php
 /* @var $this UserDetailFormController */
-/* @var $model UserDetailForm */
+/* @var $model_detail UserDetailForm */
 /* @var $form CActiveForm */
 ?>
 
 <div class="panel panel-default">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'user-detail-form-userdetail-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
 <div class="panel-body">
     <div class="form-horizontal" role="form">    
     
-    <?php 
-			$form->error($model,'Username');
-        	$form->error($model,'Name'); 
-			$form->error($model,'Email');
-			$form->error($model,'Phone');
-			$form->error($model,'Password');
-	       
-		echo $form->errorSummary($model);        
-	?>
-	<?php echo $form->hiddenField($model,'UserId') ?>
+	<?php echo $form->hiddenField($model_detail,'UserId') ?>
 	<!-- <p class="note">Fields with <span class="required">*</span> are required.</p> -->
 
-
 	<div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-        	<?php echo $form->labelEx($model,'Username', array('class'=>'control-label col-lg-2')); ?>
+        	<?php echo $form->labelEx($model_detail,'Username', array('class'=>'control-label col-lg-2')); ?>
         </div>
         <div class="col-lg-6">
-        	<?php echo $form->textField($model,'Username', array('class'=>'form-control', 'disabled' => (isset($model->UserId) && $model->UserId != "") ? 'disabled' : '')) ;?>
-            <?php if(isset($model->UserId) && $model->UserId != "") echo $form->hiddenField($model,'Username');?>
+        	<?php echo $form->textField($model_detail,'Username', array('class'=>'form-control', 'disabled' => (isset($model_detail->UserId) && $model_detail->UserId != "") ? 'disabled' : '')) ;?>
+            <?php if(isset($model_detail->UserId) && $model_detail->UserId != "") echo $form->hiddenField($model_detail,'Username');?>
         </div>
     </div>
 
 	<div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-        	<?php echo $form->labelEx($model,'Name', array('class'=>'control-label col-lg-2')); ?>
+        	<?php echo $form->labelEx($model_detail,'Name', array('class'=>'control-label col-lg-2')); ?>
         </div>
         <div class="col-lg-6">
-        	<?php echo $form->textField($model,'Name', array('class'=>'form-control')); ?>        
+        	<?php echo $form->textField($model_detail,'Name', array('class'=>'form-control')); ?>        
         </div>
     </div>
 
     <div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-        	<?php echo $form->labelEx($model,'Email', array('class'=>'control-label col-lg-2')); ?>
+        	<?php echo $form->labelEx($model_detail,'Email', array('class'=>'control-label col-lg-2')); ?>
         </div>
         <div class="col-lg-6">
-        	<?php echo $form->textField($model,'Email', array('class'=>'form-control')); ?>        
+        	<?php echo $form->textField($model_detail,'Email', array('class'=>'form-control')); ?>        
         </div>
     </div>
 
     <div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-        	<?php echo $form->labelEx($model,'Phone', array('class'=>'control-label col-lg-2')); ?>
+        	<?php echo $form->labelEx($model_detail,'Phone', array('class'=>'control-label col-lg-2')); ?>
         </div>
         <div class="col-lg-6">
-        	<?php echo $form->textField($model,'Phone', array('class'=>'form-control')); ?>        
+        	<?php echo $form->textField($model_detail,'Phone', array('class'=>'form-control')); ?>        
         </div>
     </div>
 
 	<div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-        	<?php echo $form->labelEx($model,'Password', array('class'=>'control-label col-lg-2')); ?>
+        	<?php echo $form->labelEx($model_detail,'Password', array('class'=>'control-label col-lg-2')); ?>
         </div>
         <div class="col-lg-6">
-        	<?php echo $form->PasswordField($model,'Password', array('class'=>'form-control')); ?>        
+        	<?php echo $form->PasswordField($model_detail,'Password', array('class'=>'form-control')); ?>        
         </div>
     </div>
 
 	<div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-        	<?php echo $form->labelEx($model,'Confirm_Password', array('class'=>'control-label col-lg-2')); ?>
+        	<?php echo $form->labelEx($model_detail,'Confirm_Password', array('class'=>'control-label col-lg-2')); ?>
     	</div>
         <div class="col-lg-6">
-        	<?php echo $form->PasswordField($model,'Confirm_Password', array('class'=>'form-control')); ?>        
+        	<?php echo $form->PasswordField($model_detail,'Confirm_Password', array('class'=>'form-control')); ?>        
         </div>
     </div>
 
 	<div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-            <?php echo $form->labelEx($model,'Copy_User', array('class'=>'control-label col-lg-2')); ?>
+            <?php echo $form->labelEx($model_detail,'Copy_User', array('class'=>'control-label col-lg-2')); ?>
         </div>
 		<div class="col-lg-6" style="margin-top:5px">
-	        <?php echo $form->CheckBox($model,'Copy_User',array('class'=>'check','onchange'=>'enabled_form($(\'.check\'));')); ?>
+	        <?php echo $form->CheckBox($model_detail,'Copy_User',array('class'=>'check','onchange'=>'enabled_form($(\'.check\'));')); ?>
 		</div>
     </div>
 
     <div class="form-group enable_form" style="display:none;">
         <div class="col-1"><span class="pull-right"></span>
-            <?php echo $form->labelEx($model,'User', array('class'=>'control-label col-lg-2')); ?>
+            <?php echo $form->labelEx($model_detail,'User', array('class'=>'control-label col-lg-2')); ?>
         </div>
 		<div class="col-lg-6" style="margin-top:5px">	        
-            <?php echo $form->hiddenField($model,'User', array('class'=>'User', 'style'=>'width:100%')); ?>
+            <?php echo $form->hiddenField($model_detail,'User', array('class'=>'User', 'style'=>'width:100%')); ?>
 		</div>
     </div>
 
 	<div class="form-group">
         <div class="col-1"><span class="pull-right"></span>
-            <?php echo $form->labelEx($model,'Enable', array('class'=>'control-label col-lg-2')); ?>
+            <?php echo $form->labelEx($model_detail,'Enable', array('class'=>'control-label col-lg-2')); ?>
         </div>
 		<div class="col-lg-6" style="margin-top:5px">
-	        <?php echo $form->radioButtonList($model,'Enable', array('1'=>'Yes', '0'=>'No'), array('labelOptions'=>array('style'=>'margin-right:20px;'), 'separator'=>'')); ?>
+	        <?php echo $form->radioButtonList($model_detail,'Enable', array('1'=>'Yes', '0'=>'No'), array('labelOptions'=>array('style'=>'margin-right:20px;'), 'separator'=>'')); ?>
 		</div>
     </div>
     
@@ -121,8 +104,7 @@
 	        </button>
 	    </div>
 	</div>
-        
-	<?php $this->endWidget(); ?>
+    
 
 		</div><!-- form -->
 	</div>
@@ -145,7 +127,7 @@
         query: function(query) {
             $.ajax({
                 url: "<?php echo Yii::app()->request->baseUrl;?>/user/GetUserList", 
-                data: { ajax: 1, userid: <?php echo (isset($model->UserId) && $model->UserId != "") ? $model->UserId : 0 ?>, },
+                data: { ajax: 1, userid: <?php echo (isset($model_detail->UserId) && $model_detail->UserId != "") ? $model_detail->UserId : 0 ?>, },
                 dataType: 'json',
                 type: "POST",
                 success: function(data) {
@@ -163,7 +145,7 @@
             var id = $(element).val();
             if(id !== "") {
                 $.ajax("http://localhost/Project-Management-System/user/GetUserList", {
-                    data: {id: id, ajax: 1, userid: <?php echo (isset($model->UserId) && $model->UserId != "") ? $model->UserId : 0 ?>},
+                    data: {id: id, ajax: 1, userid: <?php echo (isset($model_detail->UserId) && $model_detail->UserId != "") ? $model_detail->UserId : 0 ?>},
                     dataType: "json", type: "POST"
                 }).done(function(data) {
                     if (data.length > 0){
